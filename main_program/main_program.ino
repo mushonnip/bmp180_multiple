e8cba360683382e427a5c1829d68cd6faed89ff9
mushonnip <mushonnipabu@gmail.com>
2021-07-20 23:33:44 +0700

    update

--- main_program/main_program.ino
+++ main_program/main_program.ino
@@ -1,28 +1,84 @@
-#include "TCA9548A.h"
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <SFE_BMP180.h>
+#include <BMP180.h>
 
-TCA9548A<TwoWire> TCA;
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-SFE_BMP180 pressure1;
-SFE_BMP180 pressure2;
+BMP180 pressure1(BMP180_STANDARD);
+BMP180 pressure2(BMP180_STANDARD);
 
-#define WIRE Wire
+#define TCAADDR 0x70
+void tcaselect(uint8_t i)
+{
+  if (i > 7)
+    return;
+
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();
+}
+
+long int bmp1;
+long int bmp2;
 
 void setup()
 {
-  TCA.begin(WIRE);
-  TCA.openChannel(TCA_CHANNEL_0);
-  pressure1.begin();
-  TCA.closeChannel(TCA_CHANNEL_0);
-  TCA.openChannel(TCA_CHANNEL_1);
-  pressure2.begin();
-  TCA.closeChannel(TCA_CHANNEL_1);
+  Serial.begin(115200);
+  Wire.begin();
+
+  tcaselect(0);
+  while (pressure1.begin() != true)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print(F("Sensor BMP180 Ke-1 error"));
+
+    Serial.println(F("Sensor BMP180 Ke-1 error"));
+    delay(5000);
+  }
+
+  tcaselect(1);
+  while (pressure2.begin() != true)
+  {
+    lcd.setCursor(0, 0);
+    lcd.print(F("Sensor BMP180 Ke-2 error"));
+
+    Serial.println(F("Sensor BMP180 Ke-2 error"));
+    delay(5000);
+  }
+  tcaselect(2);
+  lcd.begin();
 }
 
 void loop()
 {
-  lcd.setCursor(0,0);
+
+  tcaselect(0);
+  bmp1 = pressure1.getPressure();
+  tcaselect(1);
+  bmp2 = pressure2.getPressure();
+  Serial.print("P1 = ");
+  Serial.print(bmp1);
+  Serial.println(" Pa");
+
+  
+  Serial.print("P2 = ");
+  Serial.print(bmp2);
+  Serial.println(" Pa");
+
+  Serial.print("P1-P2 = ");
+  Serial.print(bmp1 - bmp2);
+  Serial.println(" Pa");
+  Serial.println("");
+
+  tcaselect(2);
+  lcd.setCursor(0, 0);
+  lcd.print("NEGATIVE PRESS  ");
+  lcd.print(bmp1);
+  lcd.print("Pa");
+
   lcd.setCursor(0, 1);
+  lcd.print("P1-P2 = ");
+  lcd.print(bmp1 - bmp2);
+  lcd.print("Pa   ");
+
+  delay(1000);
 }
